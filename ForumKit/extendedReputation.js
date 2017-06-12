a4a6fe42721e44492066bddd2d25e0f221eb2182
Verschnittene <smplml1@yandex.ru>
2017-06-12 15:18:42 +0300

    Script version updated

--- ForumKit/extendedReputation.js
+++ ForumKit/extendedReputation.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reputation Revealer
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Jeveux
 // @match        http://forum.pandawow.ru/usercp.php
